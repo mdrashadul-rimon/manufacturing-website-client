a2f86f463040f2a50418333cf73cd1793426ee24
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-26 23:58:03 +0600

    update business summary stats

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -19,9 +19,9 @@ const BusinessSummary = () => {
                     <div class="stat-figure text-secondary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                     </div>
-                    <div class="stat-title">New Client This Month</div>
-                    <div class="stat-value">4,200</div>
-                    <div class="stat-desc">↗︎ 400 (22%)</div>
+                    <div class="stat-title">New Client in This Month</div>
+                    <div class="stat-value">4,270</div>
+                    <div class="stat-desc">↗︎ 430 (22%)</div>
                 </div>
 
                 <div class="stat">
