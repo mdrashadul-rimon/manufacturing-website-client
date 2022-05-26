99952d95782227c4f975f5e44c37c423802f08c1
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-26 23:54:16 +0600

    update banner page

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                     <h1 className="md:text-5xl text-2xl font-bold">Elevator Manufacturing Inc.</h1>
                     <p className="py-6 md:text-lg">Top elevator manufacturing in Asia. Presents all the elevator parts and tools with ready programmable software with proper guideline manuals.<br/></p>
                     <p className='text-accent font-semibold py-3'>We serve the best for customer needs. Explore us for your <span className='text-secondary md:text-2xl text-xl font-bold'>Business Today</span>.</p>
-                    <PrimaryButton>Explore EMI</PrimaryButton>
+                    <PrimaryButton>Explore</PrimaryButton>
                 </div>
             </div>
      
